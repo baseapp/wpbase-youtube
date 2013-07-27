8014fafde038b197af1b3b8127f760fa0b5b4fdd
vikdatta <datta_vikrant@yahoo.com>
2013-07-27 15:06:57 +0530

    commit

--- wpbase_youtube.php
+++ wpbase_youtube.php
@@ -1,34 +1,34 @@
 <?php
-/*
-   Plugin Name: wpbase-youtube
-   Plugin URI: http://wordpress.org/extend/plugins/wpbase_youtube/
-   Version: 0.1
-   Author: WPOven
-   Description: Creates a Video site using Youtube API as per user requirements.
-   Text Domain: wpbase_youtube
-   License: GPLv3
-  */
 
 /*
-    "WordPress Plugin Template" Copyright (C) 2013 Michael Simpson  (email : michael.d.simpson@gmail.com)
+  Plugin Name: wpbase-youtube
+  Plugin URI: http://wordpress.org/extend/plugins/wpbase_youtube/
+  Version: 0.1
+  Author: WPOven
+  Description: Creates a Video site using Youtube API as per user requirements.
+  Text Domain: wpbase_youtube
+  License: GPLv3
+ */
 
-    This following part of this file is part of WordPress Plugin Template for WordPress.
+/*
+  "WordPress Plugin Template" Copyright (C) 2013 Michael Simpson  (email : michael.d.simpson@gmail.com)
 
-    WordPress Plugin Template is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This following part of this file is part of WordPress Plugin Template for WordPress.
 
-    WordPress Plugin Template is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  WordPress Plugin Template is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with Contact Form to Database Extension.
-    If not, see http://www.gnu.org/licenses/gpl-3.0.html
-*/
+  WordPress Plugin Template is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
+  You should have received a copy of the GNU General Public License
+  along with Contact Form to Database Extension.
+  If not, see http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
 function get_page_by_name($pagename) {
     $pages = get_pages();
@@ -48,7 +48,7 @@ function vidfetch_video_page() {
         global $user_ID;
         $post = array();
         $post['post_type'] = 'page'; //could be 'page' for example
-        $post['post_content'] = 'This is video page';
+        $post['post_content'] = '{wpbase-youtube}';   //Shortcode to identify the page
         $post['post_author'] = '1';
         $post['post_status'] = 'publish'; //draft
         $post['post_title'] = 'Videos';
@@ -57,8 +57,8 @@ function vidfetch_video_page() {
     } else {
         // page does not exist
     }
-    
-        $page = get_page_by_name('Video');
+
+    $page = get_page_by_name('Video');
     if (empty($page)) {
         // page does not exists 
         global $user_ID;
@@ -70,6 +70,39 @@ function vidfetch_video_page() {
         $post['post_title'] = 'Video';
         $postid = wp_insert_post($post);
         add_option("video-page-id", $postid);
+    }
+
+    $page = get_page_by_name('Home');
+    if (empty($page)) {
+        // page does not exists 
+        /*     global $user_ID;
+          $post = array();
+          $post['post_type'] = 'page'; //could be 'page' for example
+          $post['post_content'] = '';
+          $post['post_author'] = '1';
+          $post['post_status'] = 'publish'; //draft
+          $post['post_title'] = 'Home';
+          $postid = wp_insert_post($post);
+          add_option("home-page-id", $postid);
+
+         */
+    }
+// Create table on plugin activation
+    global $wpdb;
+    $table_name = $wpdb->prefix . "vidfetch_history";
+    if ($wpdb->get_var("show tables like '$table_name'") != $table_name) {      //check if table already exists
+        $sql = "CREATE TABLE " . $table_name . " (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    vid_id VARCHAR(100) NOT NULL,
+    vid_title VARCHAR(200) NOT NULL,
+    type ENUM('history','downloaded'),
+    vid_rating TEXT NOT NULL,
+    vid_views int(10) NOT NULL,
+    vid_thumbnail VARCHAR(2083) DEFAULT '' NOT NULL,
+    UNIQUE KEY id (id) 
+    );";
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
     }
 }
 
@@ -83,12 +116,11 @@ $Wpbase_youtube_minimalRequiredPhpVersion = '5.0';
 function Wpbase_youtube_noticePhpVersionWrong() {
     global $Wpbase_youtube_minimalRequiredPhpVersion;
     echo '<div class="updated fade">' .
-      __('Error: plugin "wpbase-youtube" requires a newer version of PHP to be running.',  'wpbase_youtube').
-            '<br/>' . __('Minimal version of PHP required: ', 'wpbase_youtube') . '<strong>' . $Wpbase_youtube_minimalRequiredPhpVersion . '</strong>' .
-            '<br/>' . __('Your server\'s PHP version: ', 'wpbase_youtube') . '<strong>' . phpversion() . '</strong>' .
-         '</div>';
+    __('Error: plugin "wpbase-youtube" requires a newer version of PHP to be running.', 'wpbase_youtube') .
+    '<br/>' . __('Minimal version of PHP required: ', 'wpbase_youtube') . '<strong>' . $Wpbase_youtube_minimalRequiredPhpVersion . '</strong>' .
+    '<br/>' . __('Your server\'s PHP version: ', 'wpbase_youtube') . '<strong>' . phpversion() . '</strong>' .
+    '</div>';
 }
-
 
 function Wpbase_youtube_PhpVersionCheck() {
     global $Wpbase_youtube_minimalRequiredPhpVersion;
@@ -98,7 +130,6 @@ function Wpbase_youtube_PhpVersionCheck() {
     }
     return true;
 }
-
 
 /**
  * Initialize internationalization (i18n) for this plugin.
@@ -112,11 +143,9 @@ function Wpbase_youtube_i18n_init() {
     load_plugin_textdomain('wpbase_youtube', false, $pluginDir . '/languages/');
 }
 
-
 //////////////////////////////////
 // Run initialization
 /////////////////////////////////
-
 // First initialize i18n
 Wpbase_youtube_i18n_init();
 
